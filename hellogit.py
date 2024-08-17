241c393e6ea699861c7fc51e4bf25666612f61be
FelipeDiaz <felipe.diaz126@inacapmail.cl>
2024-08-17 11:03:25 -0400

    Commit primero Contador de palabras, face uno primer codigo madra Please enter the commit message for your changes. Lines starting [I

---
+++ hellogit.py
@@ -0,0 +1,10 @@
+#Cuenta palabras
+
+while True:
+    palabra = input("Ingresa una palabra (o presiona 'q' para salir): ")
+    
+    if palabra == 'q':
+        break
+    
+    cantidad_letras = len(palabra.replace(" ", ""))
+    print(f"La palabra '{palabra}' tiene {cantidad_letras} letras.")
