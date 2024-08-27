3da808b6c147280a06dffc34b720e1ae6456f315
FelipeDiaz <felipe.diaz126@inacapmail.cl>
2024-08-27 11:34:01 -0400

    Borre texto innecesario del archivo hellogit.py

--- hellogit.py
+++ hellogit.py
@@ -10,26 +10,3 @@ while True:
     
     cantidad_letras = len(palabra.replace(" ", ""))
     print(f"La palabra '{palabra}' tiene {cantidad_letras} letras.")
-
-
-#Cuenta palabras
-#producto beta
-#contador de palabra v1.0#Cuenta palabras
-#producto beta
-#contador de palabra v1.0#Cuenta palabras
-#producto beta
-#contador de palabra v1.0#Cuenta palabras
-#producto beta
-#contador de palabra v1.0#Cuenta palabras
-#producto beta
-#contador de palabra v1.0#Cuenta palabras
-#producto beta
-#contador de palabra v1.0#Cuenta palabras
-#producto beta
-#contador de palabra v1.0#Cuenta palabras
-#producto beta
-#contador de palabra v1.0#Cuenta palabras
-#producto beta
-#contador de palabra v1.0#Cuenta palabras
-#producto beta
-#contador de palabra v1.0
